fbf162a80bc3cdd0c0444742677904511c428534
gabrielkimura <gabrielkimura7@gmal.com>
2023-10-18 22:19:44 -0300

    separacao entre classes e tornando mais dinamico

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
-const offset= 0
-const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+function convertTypeToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+function convertPokemonToHtml(pokemon){
+    return `
+            <li class="pokemon">
+                <span class="number">#${pokemon.order}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${convertTypeToLi(pokemon.types).join('')}
+                    </ol>
+
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                </div>
+            </li>
+    `
+}
+
+pokeAPI.getPokemons().then((pokemons = []) => {
+
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+
+})
