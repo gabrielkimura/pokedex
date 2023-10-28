1d58463d0fb2f9a7b8a71cd1c3cf5eb3e2c64762
gabrielkimura <gabrielkimura7@gmail.com>
2023-10-28 09:53:29 -0300

    inicio implementação details pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMore')
 
+
 const maxRcords = 649;
 const limit = 10;
 let offset = 0;
@@ -9,7 +10,7 @@ let offset = 0;
 function loadPokemon(offset, limit){
     pokeAPI.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml =  pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type}">
+            <li id = ${pokemon.name} class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
@@ -29,16 +30,18 @@ loadPokemon(offset, limit)
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     const qtdRecordNexPage = offset + limit
-
     if(qtdRecordNexPage >= maxRcords){
         const newLimit = maxRcords - offset
         loadPokemon(offset, newLimit)
-
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
         loadPokemon(offset, limit)
     }
-    
 })
+
+
+
+  
+
 
 
